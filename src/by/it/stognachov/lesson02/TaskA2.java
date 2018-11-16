8093d3ab34493ac270e336f77b868a650517729b
vstognachov <vladimir.stognachov@gmail.com>
2018-11-16 16:20:00 +0300

    Homework.

--- src/by/it/stognachov/lesson02/TaskA2.java
+++ src/by/it/stognachov/lesson02/TaskA2.java
@@ -9,5 +9,11 @@ package by.it.stognachov.lesson02;
 */
 
 class TaskA2 {
-
+    public static void main(String[] args){
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+        System.out.println("Я начинаю изучать Java!");
+    }
 }
