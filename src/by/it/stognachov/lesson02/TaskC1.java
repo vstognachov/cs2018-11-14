739d22bda783e25fff83c7d4938b884e6ae1307a
vstognachov <vladimir.stognachov@gmail.com>
2018-11-16 17:00:31 +0300

    Homework.

--- src/by/it/stognachov/lesson02/TaskC1.java
+++ src/by/it/stognachov/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.stognachov.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -11,6 +13,12 @@ Sum = 60
 
 */
 class TaskC1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        s+=sc.nextInt();
+        System.out.println("Sum = "+s);
+    }
 
 
 
